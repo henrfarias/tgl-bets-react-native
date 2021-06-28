5d4b1db17c4bebbe97422ae72ba447aa2c037f7c
henrfarias <henrique.biasibetti@gmail.com>
2021-06-28 15:27:04 -0300

    feat: add google fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
 import Routes from './src/routes/index.routes';
 
 const App: React.FC = () => {
-  return (
-     <Routes />
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return <Routes />;
+};
 
 export default App;
---
+++ src/global/theme.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: '#B5C401',
+    strongText: '#707070',
+    text: '#9D9D9D',
+    background: '#F7F7F7',
+    card: '#FFF',
+    button: '#C1C1C1',
+  },
+  tipography: {
+    text: 'Roboto_400Regular',
+    title: 'Roboto_700Bold'
+  }
+}
